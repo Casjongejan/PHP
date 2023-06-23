a6b6587fb042d83a3eb6d6b9bfd5cb214e1558a4
CAs <casjongejan@gmail.com>
2023-06-23 13:44:29 +0200

    g

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+/*echo "Hello world. <br>";
+$name = 'Cas';
+$age = 15;
+echo 'Hallo mijn is: ' . $name . " en ik ben " . $age . " jaar oud. <br>";
+$try1 = 0;
+if ($try1 > 0)
+{
+    echo 'good <br>';
+}
+if ($try1 > 0 && $try1 < 10)
+{
+    echo 'good <br>';
+} else
+{
+    echo 'number out of range <br>';
+}*/
+for ($i =1; $i <= 10; $i = $i + 1){
+    echo $i . "<br>" ;
+}
