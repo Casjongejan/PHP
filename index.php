5b88ab1b33765d9ef5859fd8589be49f4cd47447
CAs <casjongejan@gmail.com>
2023-09-05 08:44:50 +0200

    Opdracht 10

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-/*echo "Hello world. <br>";
+echo "Hello world. <br>";
 $name = 'Cas';
 $age = 15;
 echo 'Hallo mijn is: ' . $name . " en ik ben " . $age . " jaar oud. <br>";
@@ -15,7 +15,28 @@ if ($try1 > 0 && $try1 < 10)
 } else
 {
     echo 'number out of range <br>';
-}*/
-for ($i =1; $i <= 10; $i = $i + 1){
+}
+for ($i =0; $i <= 10; $i = $i + 2){
     echo $i . "<br>" ;
 }
+$table = 10;
+echo '------------------------<br>';
+for ($j =1; $j <= 10; $j = $j + 1){
+    echo $j * $table . "<br>" ;
+}
+echo '------------------------<br>';
+$num1 = 0;
+$num2 = 1;
+for ($l =1; $l <= 1000; $l = $l + 1){
+    $num3 = $num2 + $num1;
+    $num1 = $num2;
+    $num2 = $num3; "<br>" ;
+
+
+}
+echo $num3;
+echo '<<br>------------------------<br>';
+$colorcode = "white";
+echo' <form action="welcome.php" method="post">
+    Color: <input type="text" Colorcode="Colorcode">';
+echo "<body style='background-color:$colorcode'>";
