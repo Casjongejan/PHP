66dbc9de0d2953bf32b8cf11a58281ef324a5f0c
CAs <casjongejan@gmail.com>
2023-09-06 15:41:06 +0200

    Opdracht 10

--- index.php
+++ index.php
@@ -16,19 +16,20 @@ if ($try1 > 0 && $try1 < 10)
 {
     echo 'number out of range <br>';
 }
-for ($i =0; $i <= 10; $i = $i + 2){
-    echo $i . "<br>" ;
+for ($i = 0; $i <= 10; $i = $i + 2)
+{
+    echo $i . "<br>";
 }
 $table = 10;
 echo '------------------------<br>';
-for ($j =1; $j <= 10; $j = $j + 1){
-    echo $j * $table . "<br>" ;
+for ($j = 1; $j <= 10; $j = $j + 1)
+{
+    echo $j * $table . "<br>";
 }
 echo '------------------------<br>';
 $num1 = 0;
 $num2 = 1;
-for ($l =1; $l <= 999; $l = $l + 1)
-//999 cuz start at 0 :p
+for ($l = 1; $l <= 999; $l = $l + 1) //999 cuz start at 0 :p
 {
     $num3 = $num2 + $num1;
     $num1 = $num2;
@@ -37,24 +38,43 @@ for ($l =1; $l <= 999; $l = $l + 1)
 }
 echo $num3;
 echo '<br>------------------------<br>';
+
+if ($_SERVER['REQUEST_METHOD'] == "POST")
 {
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-   print_r($_POST);
+    print_r($_POST["Colourcode"]);
 }
 $colourcode = implode($_POST);
-echo' <form action="index.php" method="post">
+echo ' <form action="index.php" method="post">
     color: <input type="text" name="Colourcode"><br>
     <input type="submit">';
-echo "<body style='background-color:$colourcode '>";}
+echo "<body style='background-color:$colourcode '>";
 echo '<br>------------------------<br>';
-/*if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    print_r($_POST);
+if ($_SERVER['REQUEST_METHOD'] == "POST")
+{
+    print_r($_POST["name"]);
 }
-
-
 
 echo '<br><form action="index.php" method="post">
     Name: <input type="text" name="name"><br>
 age: <input type="text" name="age"><br>
 <input type="submit">
-</form>';*/
+</form>';
+for ($k = 0; $k <= $_POST["age"]- 1; $k = $k + 1)
+{
+    echo'<br>' ,$_POST["name"] ;
+}
+echo '<br>------------------------<br>';
+if ($_SERVER['REQUEST_METHOD'] == "POST")
+{
+    print_r($_POST["Tabl"]);
+}
+
+echo ' <form action="index.php" method="post">
+    Num: <input type="text" name="Tabl"><br>
+    <input type="submit">';
+
+/*$table1 = implode($_POST["Tabl"]);*/
+for ($h = 1; $h <= 10; $h = $h + 1)
+{
+    echo $h * $_POST["Tabl"] /*$table1*/ . "<br>";
+}
